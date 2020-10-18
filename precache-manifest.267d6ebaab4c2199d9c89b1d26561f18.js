105ac38590e3928abf8ed67d52f43ea12a475133
Derrickkip <derrick.korir@gmail.com>
2020-10-18 15:32:37 +0300

    Updates

---
+++ precache-manifest.267d6ebaab4c2199d9c89b1d26561f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c5a4c83e83516b4507ffe4b271bb1e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "129df1366ae0f851cf6c",
+    "url": "/my-app/static/css/main.96d60890.chunk.css"
+  },
+  {
+    "revision": "5c57b0b294c53969f51a",
+    "url": "/my-app/static/js/2.8eeb9bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.8eeb9bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129df1366ae0f851cf6c",
+    "url": "/my-app/static/js/main.d0de0d1a.chunk.js"
+  },
+  {
+    "revision": "4f282b9659b4936d5d46",
+    "url": "/my-app/static/js/runtime-main.922ac3ad.js"
+  },
+  {
+    "revision": "9664050207bf00d574c3153f6177e0a8",
+    "url": "/my-app/static/media/image2.96640502.svg"
+  }
+]);
