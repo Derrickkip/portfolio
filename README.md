6897574c96635a59010486ead162586c69fa53d9
Derrickkip <derrick.korir@gmail.com>
2022-09-26 11:17:36 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-## My Dev Portfolio
-
-#### Built using React
-
-#### Link http://derrickkip.github.io/portfolio
---
+++ precache-manifest.f34b7a19f83f7f617c32aee91644aa2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14a4558514c424a2f0c793240fee102",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47c530dd9ea8f592fd70",
+    "url": "/portfolio/static/css/main.824fdbfa.chunk.css"
+  },
+  {
+    "revision": "901b2f433b9779c98b51",
+    "url": "/portfolio/static/js/2.5f3690b9.chunk.js"
+  },
+  {
+    "revision": "9ef29cf4785219e6cb30bf9e2569b4b7",
+    "url": "/portfolio/static/js/2.5f3690b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c530dd9ea8f592fd70",
+    "url": "/portfolio/static/js/main.6af3e7ce.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "418708e7c8d613112cf7c8ea6f06712b",
+    "url": "/portfolio/static/media/Font Awesome 5 Pro Solid.418708e7.woff"
+  },
+  {
+    "revision": "74e4688b464726e7c541a59fea3e366d",
+    "url": "/portfolio/static/media/andela.74e4688b.png"
+  },
+  {
+    "revision": "1c84753d95c3687cc335a312d7e42348",
+    "url": "/portfolio/static/media/derrick.1c84753d.jpg"
+  }
+]);
